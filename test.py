e777d3b44065ca9f58827b9f7077017ee1cbc673
Watcharin Sarachai <sarachaii@gmail.com>
2020-10-09 15:21:19 +0700

    adding test

--- test.py
+++ test.py
@@ -38,39 +38,22 @@ def configure_for_performance(ds, batch_size=32):
 
 
 if __name__ == '__main__':
-    train_data_dir = pathlib.Path("/Volumes/Data/dataset/_orchids_dataset/orchids52_data/train-en")
     test_data_dir = pathlib.Path("/Volumes/Data/dataset/_orchids_dataset/orchids52_data/test-en")
-
-    image_count = len(list(train_data_dir.glob('*/*.jpg')))
-    train_ds = tf.data.Dataset.list_files(str(train_data_dir / '*/*'), shuffle=False)
-    train_ds = train_ds.shuffle(image_count, reshuffle_each_iteration=False)
     test_ds = tf.data.Dataset.list_files(str(test_data_dir / '*/*'), shuffle=False)
-
-    class_names = np.array(sorted([item.name for item in train_data_dir.glob('*')]))
+    class_names = np.array(sorted([item.name for item in test_data_dir.glob('*')]))
     print(class_names)
-
-    val_batches = tf.data.experimental.cardinality(test_ds)
-    val_ds = test_ds.skip(val_batches // 5)
-    test_ds = test_ds.take(val_batches // 5)
-
-    print(tf.data.experimental.cardinality(train_ds).numpy())
-    print(tf.data.experimental.cardinality(val_ds).numpy())
     print(tf.data.experimental.cardinality(test_ds).numpy())
 
     num_classes = len(class_names)
     IMG_SIZE = (224, 224)
 
     # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
-    train_ds = train_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    val_ds = val_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     test_ds = test_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-    for image, label in train_ds.take(1):
+    for image, label in test_ds.take(1):
         print("Image shape: ", image.numpy().shape)
         print("Label: ", label.numpy())
 
-    train_ds = configure_for_performance(train_ds)
-    val_ds = configure_for_performance(val_ds)
     test_ds = configure_for_performance(test_ds)
 
     # Create the base model from the pre-trained model MobileNet V2
@@ -80,7 +63,7 @@ if __name__ == '__main__':
                                                    include_top=False,
                                                    weights='imagenet')
 
-    image_batch, label_batch = next(iter(train_ds))
+    image_batch, label_batch = next(iter(test_ds))
     feature_batch = base_model(image_batch)
     print(feature_batch.shape)
 
@@ -90,67 +73,26 @@ if __name__ == '__main__':
     feature_batch_average = global_average_layer(feature_batch)
     prediction_batch = prediction_layer(feature_batch_average)
 
-    data_augmentation = tf.keras.Sequential([
-        tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
-        tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-    ])
-
     preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
 
     inputs = tf.keras.Input(shape=IMG_SHAPE)
-    x = data_augmentation(inputs)
-    x = preprocess_input(x)
+    x = preprocess_input(inputs)
     x = base_model(x, training=False)
     x = global_average_layer(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
     outputs = prediction_layer(x)
     model = tf.keras.Model(inputs, outputs)
-
-    # Freeze all the layers except for dense layer
-    for layer in base_model.layers:
-        layer.trainable = False
-
-    exp_decay = False
-    if exp_decay:
-        base_learning_rate = 0.0001
-    else:
-        base_learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate=0.001,
-            decay_steps=1000,
-            decay_rate=0.96
-        )
-
-    optimizer = tf.keras.optimizers.RMSprop(learning_rate=base_learning_rate)
-
-    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  optimizer=optimizer,
-                  metrics=['accuracy'])
 
     model.summary()
 
     checkpoint_path = "/Volumes/Data/tmp/orchids-models/orchid2019/cp-{epoch:04d}.ckpt"
     checkpoint_dir = os.path.dirname(checkpoint_path)
 
-    epochs = 10
-    total_epochs = 100
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  optimizer=tf.keras.optimizers.RMSprop(),
+                  metrics=['accuracy'])
 
     latest = tf.train.latest_checkpoint(checkpoint_dir)
-    if latest:
-        model.load_weights(latest)
-    else:
-        # Save the weights using the `checkpoint_path` format
-        model.save_weights(checkpoint_path.format(epoch=0))
+    model.load_weights(latest)
 
-    # Create a callback that saves the model's weights
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                     save_weights_only=True,
-                                                     verbose=1)
-
-    history_fine = model.fit(train_ds,
-                             epochs=total_epochs,
-                             validation_data=val_ds,
-                             callbacks=[cp_callback],
-                             initial_epoch=epochs)
-
-    loss, accuracy = model.evaluate(test_ds, verbose=2)
+    loss, accuracy = model.evaluate(test_ds)
     print('Test accuracy :', accuracy)
