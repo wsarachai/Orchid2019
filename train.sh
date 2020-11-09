fa36cd6bd5783fd07c880ed7dd8025cee4d7adc9
Watcharin <sarachaii@gmail.com>
2020-11-09 15:19:02 +0700

    train script

---
+++ train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ROOT_PATH="${WORKSPACE}"
+MODELS_DIR="${ROOT_PATH}/orchids-models"
+TRAIN_DIR=$PWD
+
+echo "${TRAIN_DIR}"
+
+file="${TRAIN_DIR}/train.out"
+if [[ -f "$file" ]]; then
+    echo "delete ${file}"
+	rm ${file}
+fi
+
+script="${TRAIN_DIR}/train.py"
+if [[ -f "${script}" ]]; then
+	echo "runing for script: ${script}"
+	nohup python "${script}" > train.out 2>&1 &
+    echo $! > train_pid.txt
+else
+	echo "Invalid script filename."
+fi
