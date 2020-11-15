dc00e012ced7d2ea80be507341305c1c03c9b2f6
Watcharin <sarachaii@gmail.com>
2020-11-15 15:02:10 +0700

    Change training step

--- train.sh
+++ train.sh
@@ -15,7 +15,7 @@ fi
 script="${TRAIN_DIR}/train.py"
 if [[ -f "${script}" ]]; then
 	echo "running for script: ${script}"
-	nohup python "${script}" "--bash=True" "--start_state=1" "--aug_method=slow" "--learning_rate=0.001" "--total_epochs=100,200,200,500" > train.out 2>&1 &
+	nohup python "${script}" "--bash=True" "--start_state=1" "--end_state=5" "--dataset=orchids52_v1_tf" "--model=mobilenet_v2_140_orchids52" "--learning_rate=0.001" "--total_epochs=100,100,20,100" --aug_method=slow > train.out 2>&1 &
     echo $! > train_pid.txt
 else
 	echo "Invalid script filename."
