1efbb46d6f9ddf6f31f19465b228718fb21f4fdb
Watcharin Sarachai <sarachaii@gmail.com>
2021-06-27 22:31:44 +0700

    flowers17, flowers102 stn test

--- nets/utils.py
+++ nets/utils.py
@@ -15,14 +15,21 @@ TRAIN_V2_STEP1 = 'v2-pretrain1'
 TRAIN_V2_STEP2 = 'v2-pretrain2'
 
 MOBILENET_V2_140 = 'mobilenet_v2_140'
+MOBILENET_V2_140_FLOWERS17 = 'mobilenet_v2_140_flowers17'
+MOBILENET_V2_140_FLOWERS102 = 'mobilenet_v2_140_flowers102'
 MOBILENET_V2_140_ORCHIDS52 = 'mobilenet_v2_140_orchids52'
 
 
 nets_mapping = {
     MOBILENET_V2_140: create_mobilenet_v2_14,
-    MOBILENET_V2_140_ORCHIDS52: create_orchid_mobilenet_v2_15
+    MOBILENET_V2_140_FLOWERS17: create_orchid_mobilenet_v2_15,
+    MOBILENET_V2_140_FLOWERS102: create_orchid_mobilenet_v2_15,
+    MOBILENET_V2_140_ORCHIDS52: create_orchid_mobilenet_v2_15,
 }
 
 preprocessing_mapping = {
-    'mobilenet_v2_140': preprocess_input,
+    MOBILENET_V2_140: preprocess_input,
+    MOBILENET_V2_140_FLOWERS17: preprocess_input,
+    MOBILENET_V2_140_FLOWERS102: preprocess_input,
+    MOBILENET_V2_140_ORCHIDS52: preprocess_input,
 }
