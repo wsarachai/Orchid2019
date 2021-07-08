d9e0fd7d31f458bd1fc60ec5db859ad679f863a5
Watcharin Sarachai <sarachaii@gmail.com>
2021-07-08 13:40:24 +0700

    Some testing code

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
 import numpy as np
 import tensorflow as tf
 
-
 if __name__ == '__main__':
-    var = tf.Variable(np.random.random(size=(1,)))
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-    def lr_callable():
-        return .1
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-    opt = tf.keras.optimizers.SGD(learning_rate=lr_callable)
-    loss = lambda: 3 * var
-    opt.minimize(loss, var_list=[var])
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=10)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+    print('\nTest accuracy:', test_acc)
+
+    probability_model = tf.keras.Sequential([model,
+                                             tf.keras.layers.Softmax()])
+
+    predictions = probability_model.predict(test_images)
+
+    np.argmax(predictions[0])
